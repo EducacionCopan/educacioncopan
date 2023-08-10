10762ad987bfbae02737888ce6de40b0e657e37a
EducacionCopan <secretariaeducacioncopan@gmail.com>
2023-08-10 16:35:46 -0600

    quitar duplicados footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,10 +33,8 @@ export const Footer = () => {
           <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
             <h6 className="text-uppercase mb-4 font-weight-bold">
               {values.titulo}
-              {values.titulo}
             </h6>
             <p>
-            {values.footerDesc}
             {values.footerDesc}
             </p>
           </div>
@@ -68,9 +66,6 @@ export const Footer = () => {
 
           <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
             <h6 className="text-uppercase mb-4 font-weight-bold">Contacto</h6>
-            <p><i className="fas fa-home mr-3"></i>{values.footerDireccion}</p>
-            <p><i className="fas fa-envelope mr-3"></i>{values.footerCorreo}</p>
-            <p><i className="fas fa-phone mr-3"></i>{values.footerTelefonos}</p>
             <p><i className="fas fa-home mr-3"></i>{values.footerDireccion}</p>
             <p><i className="fas fa-envelope mr-3"></i>{values.footerCorreo}</p>
             <p><i className="fas fa-phone mr-3"></i>{values.footerTelefonos}</p>
@@ -124,9 +119,9 @@ export const Footer = () => {
       </div>
     </div>
     <div
-         className="text-center p-3"
-         style={{backgroundColor: "var(--mp-rojo-1)"}}
-         >
+        className="text-center p-3"
+        style={{backgroundColor: "var(--mp-rojo-1)"}}
+        >
       © 2023 Copyright:
       <br /><a className="text-white" href={values.footerEnlace}>{values.subtitulo}</a>
     </div>
